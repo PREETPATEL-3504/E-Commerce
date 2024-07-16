3066c9866b2112b1f9f5857bb51e01a6c25a1fe6
Brijesh Borad <brijeshborad29@gmail.com>
2024-07-16 10:34:04 +0530

    Add Image required condition

--- frontend/src/components/addProducts.tsx
+++ frontend/src/components/addProducts.tsx
@@ -77,7 +77,7 @@ const AddProducts = () => {
         <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
           <div className="relative mx-4 -mt-6 mb-4 grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-cyan-600 to-cyan-400 bg-clip-border text-white shadow-lg shadow-cyan-500/40">
             <h3 className="block font-sans text-3xl font-semibold leading-snug tracking-normal text-white antialiased">
-              Add Products
+              {id ? "Update Products" : "Add Products"}
             </h3>
           </div>
           <form onSubmit={handleSubmit}>
@@ -147,23 +147,41 @@ const AddProducts = () => {
               </div>
 
               <div className="relative h-11 w-full min-w-[200px]">
-                <input
-                  type="file"
-                  pattern="[A-Za-z]{2,}"
-                  required
-                  accept="image/png, image/svg, image/jpeg"
-                  onChange={(e) => {
-                    if (e.target.files && e.target.files.length > 0) {
-                      setData({
-                        ...data,
-                        image_url: window.URL.createObjectURL(
-                          e.target.files[0]
-                        ),
-                      });
-                    }
-                  }}
-                  className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                />
+                {id ? (
+                  <input
+                    type="file"
+                    accept="image/png, image/svg, image/jpeg"
+                    onChange={(e) => {
+                      if (e.target.files && e.target.files.length > 0) {
+                        setData({
+                          ...data,
+                          image_url: window.URL.createObjectURL(
+                            e.target.files[0]
+                          ),
+                        });
+                      }
+                    }}
+                    className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  />
+                ) : (
+                  <input
+                    type="file"
+                    accept="image/png, image/svg, image/jpeg"
+                    required
+                    onChange={(e) => {
+                      if (e.target.files && e.target.files.length > 0) {
+                        setData({
+                          ...data,
+                          image_url: window.URL.createObjectURL(
+                            e.target.files[0]
+                          ),
+                        });
+                      }
+                    }}
+                    className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-cyan-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                  />
+                )}
+
                 <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-cyan-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-cyan-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-cyan-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                   Product Image
                 </label>
