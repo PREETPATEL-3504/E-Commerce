0d6fe22d8706e700f69a19fc143e813058a2360a
Akshay Patel <axaypatel2345@gmail.com>
2024-08-10 18:08:57 +0530

    Formtedd

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,8 @@ const db = require("./db");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { app, server } = require("./socket");
-const cron = require("node-cron");
+const cron = require("./services/cron");
 const router = require("./routes/index");
-const XLSX = require("xlsx");
-const mail = require("./services/mail");
-const readExcelFile = require("./services/readfile");
 
 const corsOption = {
   origin: "http://localhost:3000",
@@ -23,20 +20,6 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Routes
 app.use("/", router);
-
-// Schedule the task
-cron.schedule("0 0 * * *", () => {
-  const data = readExcelFile(
-    "/home/admin1/Documents/Preet/E-Commerce/E-Commerce/backend/data.xlsx"
-  );
-  for (let i = 0; i < data.length; i++) {
-    if (data[i].status == "failed") {
-      const subject = "Your payment was failed!";
-      const body = "Dear user your payment failed";
-      mail(data[i].email, subject, body);
-    }
-  }
-});
 
 server.listen(5000, () => {
   console.log("Server is running on port 5000");
--- backend/services/cron.js
+++ backend/services/cron.js
@@ -1,5 +1,15 @@
-const cron = require('node-cron');
+const cron = require("node-cron");
+const readExcelFile = require("./readfile");
 
-cron.schedule('* * * * *', () => {
-  console.log('running a task every minute');
+cron.schedule("* * * * *", () => {
+  const data = readExcelFile(
+    "/home/admin1/Documents/Preet/E-Commerce/E-Commerce/backend/data.xlsx"
+  );
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].status == "failed") {
+      const subject = "Your payment was failed!";
+      const body = "Dear user your payment failed";
+      mail(data[i].email, subject, body);
+    }
+  }
 });
